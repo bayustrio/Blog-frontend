709b849b1dc108257ba3457832f7b66998c15ae7
muhammad satrio <bayustrio7@gmail.com>
2022-03-28 16:29:01 +0700

    feat: change css

--- pages/Navbar/Navbar.tsx
+++ pages/Navbar/Navbar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from "next/link";
-import styles from "../../styles/navbar.module.scss";
+import styles from "../../styles/navbar.module.css";
 // COOKIES
 import Cookies from "js-cookie";
 import Router from "next/router";
